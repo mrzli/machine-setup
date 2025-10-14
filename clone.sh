cab90ce36c0aad7854a108d34e1ff380b50e5850
Goran Mržljak <goran.mrzljak@gmail.com>
2025-10-14 21:31:41 +0200

    update

---
+++ clone.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO_URL="https://github.com/mrzli/machine-setup.git"
+
+XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+
+if ! command -v git &>/dev/null; then
+  echo "Git not found. Installing git..."
+  sudo pacman -Sy git --noconfirm
+fi
+
+mkdir -p "$XDG_DATA_HOME"
+
+cd "$XDG_DATA_HOME"
+git clone "$REPO_URL"
