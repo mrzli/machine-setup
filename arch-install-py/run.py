48a8119099e86d2a000093c8e53ed642b90d7b9c
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-13 08:48:49 +0100

    update

--- arch-install-py/run.py
+++ arch-install-py/run.py
@@ -2,12 +2,32 @@
 
 from util import input_password, get_block_device_names
 
-devices = get_block_device_names()
-print("Available block devices:")
-for device in devices:
-    print(f"- {device}")
+def choose_block_device():
+    devices = get_block_device_names()
+    if not devices:
+        print("No block devices found. Exiting.")
+        exit(1)
 
-disk_name = input("Disk name: ")
+    print("Available block devices:")
+    for i, name in enumerate(devices, 1):
+        print(f"{i}. {name}")
+
+    disk_name = None
+
+    while True:
+        try:
+            choice = int(input("Select disk number: ")) - 1
+            if 0 <= choice < len(devices):
+                disk_name = devices[choice]
+                break
+            else:
+                print("Invalid number. Please try again.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+    return disk_name
+
+disk_name = choose_block_device()
 root_partition_password = input_password("Root partition password: ")
 username = input("Username: ")
 user_password = input_password("User password: ")
