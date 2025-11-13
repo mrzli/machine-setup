023ff418da36fa9af398f258c2311552a8b19d24
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-13 12:02:15 +0100

    update

--- arch-install-py/run.py
+++ arch-install-py/run.py
@@ -1,7 +1,9 @@
 from pprint import pprint
-from steps import process_installation_inputs
+from steps import process_installation_inputs, setup_disk
 
 inputs = process_installation_inputs()
 
 print("Installation Inputs Collected:")
 pprint(inputs)
+
+setup_disk(inputs)
--- arch-install-py/steps/__init__.py
+++ arch-install-py/steps/__init__.py
@@ -1,5 +1,7 @@
 from .s01_input import process_installation_inputs
+from .s02_disk import setup_disk
 
 __all__ = [
-    "process_installation_inputs"
+    "process_installation_inputs",
+    "setup_disk"
 ]
