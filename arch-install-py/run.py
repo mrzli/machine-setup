dc9fee3181bebf0edaff249f9a99ae4c91c55207
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-13 08:28:54 +0100

    update

--- arch-install-py/run.py
+++ arch-install-py/run.py
@@ -3,10 +3,22 @@
 import getpass
 import subprocess
 
-disk_name = input("Disk name: ")
-root_partition_password = getpass.getpass("Root partition password: ")
-root_partition_password_confirm = getpass.getpass("Confirm password: ")
+def enter_password(propmpt):
+    while True:
+        password = getpass.getpass(prompt)
 
+        if not password:
+            print("Password cannot be empty. Please try again.")
+            continue
+
+        password_confirm = getpass.getpass("Confirm password: ")
+        if password != password_confirm:
+            print("Passwords do not match. Please try again.")
+            continue
+
+        return password
+
+disk_name = input("Disk name: ")
+root_partition_password = enter_password("Root partition password: ")
 username = input("Username: ")
-user_password = getpass.getpass("User password: ")
-user_password_confirm = getpass.getpass("Confirm password: ")
+user_password = enter_password("User password: ")
