330762442b08f24e30ef5f5bd1ae904a754bdd07
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-13 09:08:48 +0100

    update

--- arch-install-py/util/__init__.py
+++ arch-install-py/util/__init__.py
@@ -1,8 +1,7 @@
-from .input import choose_block_device, input_password
+from .input import input_password
 from .linux import get_block_device_names
 
 __all__ = [
-    "choose_block_device",
     "input_password",
     "get_block_device_names"
 ]
