3f6dfbae77943a68b788e2bb88f2713589a94355
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-13 17:02:46 +0100

    update

--- arch-install-py/steps/s03_chroot_prereq.py
+++ arch-install-py/steps/s03_chroot_prereq.py
@@ -1,2 +1,16 @@
+from util import command
+
 def setup_chroot_prerequisites(inputs):
-    pass
+    print("Setup chroot prerequisites...\n")
+
+    print("Installing essential packages...")
+    essential_packages = [
+        "base",
+        "iptables-nft",
+    ]
+    command(["pacstrap", "/mnt", *essential_packages])
+
+    print("Generating fstab file...")
+    command(["genfstab -U /mnt >> /mnt/etc/fstab"], shell=True)
+
+    print("\nFinished setting up chroot prerequisites.\n")
