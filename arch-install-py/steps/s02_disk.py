7928f13e115b91c4f8b60e53294e088de23b2a8b
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-13 12:27:50 +0100

    update

--- arch-install-py/steps/s02_disk.py
+++ arch-install-py/steps/s02_disk.py
@@ -13,7 +13,7 @@ def setup_disk(inputs):
 
     print(f"Wiping disk '{device_name}'...")
     clear_disk(inputs)
-    subprocess.run(["wipefs", "-a", device_name], capture_output=False)
+    subprocess.run(["wipefs", "-a", device_name], capture_output=False, check=True)
 
     # Create partitions.
     print(f"Creating new GPT partition table on '{device_name}'...")
