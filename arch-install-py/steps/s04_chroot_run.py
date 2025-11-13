dfc2449ab868d5552166f974417cfc0f99d2dd8a
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-13 18:25:29 +0100

    update

--- arch-install-py/steps/s04_chroot_run.py
+++ arch-install-py/steps/s04_chroot_run.py
@@ -1,2 +1,34 @@
+from util import command
+import shutil
+
 def chroot_run(inputs):
-    pass
+    username = inputs.username
+    user_password = inputs.user_password
+    device_partition_efi = inputs.device_partition_efi
+    device_partition_root = inputs.device_partition_root
+    vol_group_name = inputs.vol_group_name
+
+    command(["mkdir", "-p", "/mnt/arch-install"])
+
+    # Copy files from chroot dir, which is in parent of this script dir
+    # use python functions
+    # so this script then "../chroot", then copy all from there to "/mnt/arch-install"
+    script_dir = shutil.os.path.dirname(shutil.os.path.abspath(__file__))
+    chroot_dir = shutil.os.path.abspath(shutil.os.path.join(script_dir, "..", "chroot"))
+    shutil.copytree(chroot_dir, "/mnt/arch-install", dirs_exist_ok=True)
+
+    chroot_cmd = (
+        "pacman -Syu --noconfirm python && "
+        "cd /arch-install && "
+        'python script.py "{}" "{}" "{}" "{}" "{}"'.format(
+            username,
+            user_password,
+            device_partition_efi,
+            device_partition_root,
+            vol_group_name
+        )
+    )
+
+    command(["arch-chroot", "/mnt", "/bin/bash", "-c", chroot_cmd], output='all')
+
+    command(["rm", "-rf", "/mnt/arch-install"])
