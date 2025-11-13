d9e89fbe193794ae4bf9be5f18c605d2f3f9620e
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-13 20:50:14 +0100

    update

--- arch-install-py/util/__init__.py
+++ arch-install-py/util/__init__.py
@@ -1,12 +1,15 @@
 from .input import input_password
 from .linux import get_block_device_names, command
 from .logger import LogLevel, Logger, LoggerConsoleHandler, LoggerFileHandler
-from .output import log, print_separator
+from .output import print_separator
 
 __all__ = [
     "input_password",
     "get_block_device_names",
     "command",
-    "log",
+    "LogLevel",
+    "Logger",
+    "LoggerConsoleHandler",
+    "LoggerFileHandler",
     "print_separator"
 ]
