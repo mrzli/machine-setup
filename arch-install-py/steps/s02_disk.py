6a8a63aabf7e199d0d0b6f1df0b0cad96e690412
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-13 12:28:57 +0100

    update

--- arch-install-py/steps/s02_disk.py
+++ arch-install-py/steps/s02_disk.py
@@ -17,52 +17,49 @@ def setup_disk(inputs):
 
     # Create partitions.
     print(f"Creating new GPT partition table on '{device_name}'...")
-    subprocess.run(f'echo "label: gpt" | sfdisk "{device_name}" ', shell=True, capture_output=True)
+    subprocess.run(f'echo "label: gpt" | sfdisk "{device_name}" ', shell=True, capture_output=True, check=True)
 
     print(f"Creating 1GB EFI partition on '{device_name}'...")
-    subprocess.run(f'echo "size=1GiB,type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B" | sfdisk --append "{device_name}" ', shell=True, capture_output=True)
+    subprocess.run(f'echo "size=1GiB,type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B" | sfdisk --append "{device_name}" ', shell=True, capture_output=True, check=True)
 
     print(f"Creating LVM partition on the rest of space on '{device_name}'...")
-    subprocess.run(f'echo "size=+,type=E6D6D379-F507-44C2-A23C-238F2A3DF928" | sfdisk --append "{device_name}" ', shell=True, capture_output=True)
+    subprocess.run(f'echo "size=+,type=E6D6D379-F507-44C2-A23C-238F2A3DF928" | sfdisk --append "{device_name}" ', shell=True, capture_output=True, check=True)
 
     # Set up root partition.
     print(f"Setting up LUKS encryption on root partition '{device_partition_root}'...")
-    subprocess.run(f'echo {root_partition_password} | cryptsetup luksFormat --batch-mode "{device_partition_root}"', shell=True, capture_output=True)
+    subprocess.run(f'echo {root_partition_password} | cryptsetup luksFormat --batch-mode "{device_partition_root}"', shell=True, capture_output=True, check=True)
 
     print(f"Opening LUKS encrypted root partition '{device_partition_root}' as '{lvm_name}'...")
-    subprocess.run(f'echo {root_partition_password} | cryptsetup open --type luks "{device_partition_root}" "{lvm_name}"', shell=True, capture_output=True)
+    subprocess.run(f'echo {root_partition_password} | cryptsetup open --type luks "{device_partition_root}" "{lvm_name}"', shell=True, capture_output=True, check=True)
 
     print(f"Creating physical volume targetting '/dev/mapper/{lvm_name}'...")
-    subprocess.run(["pvcreate", f"/dev/mapper/{lvm_name}"], capture_output=True)
+    subprocess.run(["pvcreate", f"/dev/mapper/{lvm_name}"], capture_output=True, check=True)
 
     print(f"Creating volume group '{vol_group_name}' targetting '/dev/mapper/{lvm_name}'...")
-    subprocess.run(["vgcreate", vol_group_name, f"/dev/mapper/{lvm_name}"], capture_output=True)
+    subprocess.run(["vgcreate", vol_group_name, f"/dev/mapper/{lvm_name}"], capture_output=True, check=True)
 
     print(f"Creating logical volume '{lv_name}' in volume group '{vol_group_name}' with all available space...")
-    subprocess.run(["lvcreate", "-l", "100%VG", "-n", lv_name, vol_group_name])
+    subprocess.run(["lvcreate", "-l", "100%VG", "-n", lv_name, vol_group_name], capture_output=False, check=True)
 
     print("Activating all volume groups...")
-    subprocess.run(["vgchange", "-ay"], capture_output=True)
+    subprocess.run(["vgchange", "-ay"], capture_output=True, check=True)
 
     # Format partitions.
     print(f"Formatting EFI partition '{device_partition_efi}' as FAT32...")
-    subprocess.run(["mkfs.fat", "-F32", device_partition_efi], capture_output=True)
-
+    subprocess.run(["mkfs.fat", "-F32", device_partition_efi], capture_output=True, check=True)
     root_lv = f"/dev/{vol_group_name}/{lv_name}"
 
     print(f"Formatting root logical volume '{root_lv}' as ext4...")
-    subprocess.run(["mkfs.ext4", root_lv], capture_output=True)
-
+    subprocess.run(["mkfs.ext4", root_lv], capture_output=True, check=True)
     print("")
 
     # Mount partitions.
     print(f"Mounting root logical volume '{root_lv}' to '/mnt'...")
-    subprocess.run(["mount", root_lv, "/mnt"], capture_output=True)
+    subprocess.run(["mount", root_lv, "/mnt"], capture_output=True, check=True)
 
     print(f"Mounting EFI partition '{device_partition_efi}' to '/mnt/boot'...")
-    subprocess.run(["mkdir", "-p", "/mnt/boot"], capture_output=True)
-    subprocess.run(["mount", device_partition_efi, "/mnt/boot"], capture_output=True)
-
+    subprocess.run(["mkdir", "-p", "/mnt/boot"], capture_output=True, check=True)
+    subprocess.run(["mount", device_partition_efi, "/mnt/boot"], capture_output=True, check=True)
     print("\nDisk setup completed successfully.\n")
 
 
