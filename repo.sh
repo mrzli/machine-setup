45558a06588876b45f13166162e8baba46f54832
Goran Mržljak <goran.mrzljak@gmail.com>
2025-11-17 00:33:50 +0100

    remove some left over scripts from old bash installation flow

--- repo.sh
+++ repo.sh
@@ -40,8 +40,5 @@ echo -e "\nMaking scripts executable..."
 
 # Make scripts executable.
 find "$local_repo_path/scripts" -mindepth 1 -type f -exec chmod +x {} \;
-chmod +x "$local_repo_path/repo.sh"
-chmod +x "$local_repo_path/arch-install.sh"
-find "$local_repo_path/arch-install" -mindepth 1 -type f -name "*.sh" -exec chmod +x {} \;
 
 echo -e "Done.\n\n"
